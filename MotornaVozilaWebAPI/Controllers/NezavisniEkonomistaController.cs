e9edb2b984a699d8c166a3dab41dc4c391da0c4f
UrosVel <velickovic97@gmail.com>
2021-06-11 14:35:29 +0200

    Test

---
+++ MotornaVozilaWebAPI/Controllers/NezavisniEkonomistaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MotornaVozilaLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotornaVozilaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NezavisniEkonomistaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("VratiNezavisneEkonomiste")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetNezavisniEkonomista()
+        {
+            try
+            {
+                return new JsonResult(DataProvider.VratiNezavisneEkonomiste());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
