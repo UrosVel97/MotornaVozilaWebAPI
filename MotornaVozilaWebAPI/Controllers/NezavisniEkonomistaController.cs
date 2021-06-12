cbf4b104bf9c99e3481b172063249368b93d783e
UrosVel <velickovic97@gmail.com>
2021-06-12 17:59:55 +0200

    Komentar 2

--- MotornaVozilaWebAPI/Controllers/NezavisniEkonomistaController.cs
+++ MotornaVozilaWebAPI/Controllers/NezavisniEkonomistaController.cs
@@ -87,6 +87,15 @@ namespace MotornaVozilaWebAPI.Controllers
         {
             try
             {
+
+
+                /*
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 */
                 DataProvider.AzurirajNezavisneEkonomiste(nezavisni);
                 return Ok();
             }
