1212cf0be7854747c361f6714ce8d3b01e2bd7ec
Goran Dimcic <dimcic.goran@hotmail.com>
2021-06-20 20:25:46 +0200

    Salon

--- MotornaVozilaWebAPI/Controllers/SalonController.cs
+++ MotornaVozilaWebAPI/Controllers/SalonController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MotornaVozilaLibrary;
+using MotornaVozilaLibrary.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,36 @@ namespace MotornaVozilaWebAPI.Controllers
     [Route("[controller]")]
     public class SalonController : ControllerBase
     {
+        [HttpGet]
+        [Route("VratiSalone")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSaloni()
+        {
+            try
+            {
+                return new JsonResult(DataProvider.VratiSalone());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
 
+        [HttpPut]
+        [Route("PromeniSalone")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult AzurirajSalone([FromBody] SalonView salon)
+        {
+            try
+            {
+                DataProvider.AzurirajSalone(salon);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
     }
 }
